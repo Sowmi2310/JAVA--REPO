eb163227163b93cbdd70f1e9f15e540fce1eae0d
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-11-14 21:10:01 +0530

    JAVA Task

---
+++ Restaurant.java
@@ -0,0 +1,22 @@
+class Restaurant
+{
+	private String food;
+	private int price;
+	
+	String getFood()
+	{
+		return food;
+	}
+	void setFood(String food)
+	{
+		this.food=food;
+	}
+	int getPrice()
+	{
+		return price;
+	}
+	void setPrice(int price)
+	{
+		this.price=price;
+	}
+}
