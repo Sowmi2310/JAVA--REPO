4741c9af90c761452dd89850f163e9bc07cc8bc9
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-11-14 20:52:59 +0530

    JAVA Task

---
+++ Encapsulation.java
@@ -0,0 +1,11 @@
+class Encapsulation
+{
+	public static void main(String adg[])
+	{
+		Restaurant mu=new Restaurant();
+		mu.setFood("biriyani");
+		System.out.println(mu.getFood());
+		mu.setPrice(230);
+		System.out.println(mu.getPrice());
+	}
+}
