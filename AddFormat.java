20f4ec63ca85612df73ba6d0642c0377a1e92ad5
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-11-14 15:07:42 +0530

    JAVA Task

---
+++ AddFormat.java
@@ -0,0 +1,25 @@
+class AddFormat
+{
+	public static void main(String arr[])
+	{
+		int a=1784;
+		int b=9865;
+		int c=4262;
+		if(a>b)
+		{
+			System.out.println("the greater num is:"+a);
+		}
+		else if(b>c)
+		{
+			System.out.println("the greater num is:"+b);
+		}
+		else if(a>c)
+		{
+			System.out.println("the greater num is:"+c);
+		}
+		else
+		{
+			System.out.println("num is not correct");
+		}
+	}
+}
