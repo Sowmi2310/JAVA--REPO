e40c5c8aa468a62f4198d0301a973a6b8ad30640
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-11-14 20:53:42 +0530

    JAVA Task

---
+++ EvenNumber.java
@@ -0,0 +1,19 @@
+class EvenNumber
+{
+	public static void main(String arg[])
+	{
+		for(int i=1;i<=30;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("EVEN="+i);
+			}
+			else
+			{
+				System.out.println("ODD="+i);
+			}
+			
+		}
+	}
+}
+		
