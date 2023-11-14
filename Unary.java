8d3d4d12069852e00fafb5f58e034aa28f83165e
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-11-14 21:22:50 +0530

    JAVA Task

---
+++ Unary.java
@@ -0,0 +1,29 @@
+class Unary
+{
+	public static void main(String agrs[])
+	{
+		//int a=10;
+		//int b=++a;
+		//System.out.println(b);
+              	//int b=++a;
+		//int b=--a;
+		//int b=~a;
+		//int b=~-a;
+		//int b=a++ + ++a;
+		//int b=a++ + 1;
+		//int b=a-- - --a;
+		/*System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(a--);
+		System.out.println(--a);*/
+		//System.out.println(b);
+		int a=10*10/5+3-1*4/2;
+		System.out.println(a);
+		int b=20;
+		System.out.println(b<<4);
+		System.out.println(20>>2);
+		System.out.println(20>>>2);
+		
+		
+	}
+}
