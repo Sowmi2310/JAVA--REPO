15996b08b22b52659092703807c6cceeb99519f6
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-11-14 15:14:57 +0530

    JAVA Task

---
+++ Change.java
@@ -0,0 +1,33 @@
+class Change
+{
+
+    void user()
+           {
+
+             String a="welcome";
+             System.out.println(a);
+           } 
+    void get()
+          {
+          int a=200;
+          a=10;
+          int b=100;
+          int c=a+b;
+          System.out.println(c);
+          }    
+
+    public static void main(String... arr)
+           {
+
+            System.out.println("JAVA");
+            //Change b=new Change();
+            //Change d=new Change();
+            //b.user();
+            //b.get();
+             
+            System.out.print("ALL");
+          
+           }
+
+
+}
