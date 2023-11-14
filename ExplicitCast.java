7c5ff073a81784602cd0e1598a2db46bc9cfc115
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-11-14 20:54:38 +0530

    JAVA Task

---
+++ ExplicitCast.java
@@ -0,0 +1,22 @@
+class ExplicitCast
+{ 
+	public static void main(String ass[])
+	{
+		int a=100;//implict
+		System.out.println(a);
+		byte b=(byte)600;
+		System.out.println(b);
+		 
+		float f=78.67f;
+		System.out.println(f);
+
+		int c=(int)34.89;//explicit
+		System.out.println(c);
+
+		//byte i=128;  //  not enough space so throw error
+		//System.out.println(i);
+
+
+
+	}
+}	
