542e9f6e9af523642e9c1b1cca637a4dfe5b6e11
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-11-14 21:15:35 +0530

    JAVA Task

---
+++ Sorting.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Sorting
+{
+	public static void main(String rss[])throws Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a word:");
+		String a=sc.nextLine();
+		//int b=a.length();
+		for(int i=0;i<=a.length();i++)
+		{
+			System.out.println(a.charAt(i));
+		}
+		
+	}
+}
